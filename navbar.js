db1cbf3a7183aaa908d0c632aa6d97177f21b90a
charbel648 <charbel.faddoul@net.usj.edu.lb>
2025-07-04 14:08:03 +0300

    Initial commit

---
+++ navbar.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("navbar.html")
+      .then(response => {
+        if (!response.ok) throw new Error("Network response was not ok");
+        return response.text();
+      })
+      .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+      })
+      .catch(error => {
+        console.error("Error loading navbar:", error);
+      });
+
+  });
+  
+  
