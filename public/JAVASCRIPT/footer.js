5a49f0813f441fd0d86ab95f714bd04d5da7adf8
charbel648 <charbel.faddoul@net.usj.edu.lb>
2025-07-08 00:28:38 +0300

    finished product

--- public/JAVASCRIPT/footer.js
+++ public/JAVASCRIPT/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("HTML/footer.html")
+    fetch("../HTML/footer.html")
       .then(response => {
         if (!response.ok) throw new Error("Network response was not ok");
         return response.text();
